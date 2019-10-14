a734b8482acaf52c0cdc8d16a12adebb9aa31b00
joshua <mama27j@gmail.com>
2019-10-14 21:25:42 +0300

    fix lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {LandingComponent} from './landing/landing.component';
 import {LoginComponent} from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {SaleComponent} from './sales-module/sale/sale.component';
-import {WholeSaleComponent} from './sales-module/whole-sale/whole-sale.component';
 import {PurchaseComponent} from './purchase/purchase.component';
 
 const routes: Routes = [
   {path: '', component: LandingComponent},
   {path: 'admin', component: DashboardComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'sale', loadChildren: () => import('./sales-module/sales-module.module').then(m => m.SalesModuleModule)},
-  {path: 'stock', loadChildren: () => import('./stock-module/stock-module.module').then(m => m.StockModuleModule)},
+  {path: 'sale', loadChildren: './sales-module/sales-module.module#SalesModuleModule'},
+  {path: 'stock', loadChildren: './stock-module/stock-module.module#StockModuleModule'},
   {path: 'purchase', component: PurchaseComponent},
   {path: 'home', redirectTo: ''},
 ];
