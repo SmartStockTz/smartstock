40eacf598de8107660983d19e3a9493335b17c60
joshua <mama27j@gmail.com>
2019-12-28 01:51:18 +0300

    start to build desktop app

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require('electron');
+const url = require("url");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1100,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  });
+
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `./dist/lbpharmacy/index.html`),
+      protocol: "file:",
+      slashes: true,
+    })
+  );
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.commandLine.appendSwitch('allow-insecure-localhost', 'true');
+app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
