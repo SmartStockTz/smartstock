bb1a8347038696472f62bf1b2c160caeee47f958
joshua <mama27j@gmail.com>
2020-05-25 15:40:21 +0300

    refresh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,33 @@ import {AuthenticationGuard} from './guards/authentication.guard';
 import {StockExistGuard} from './guards/stock-exist.guard';
 import {ActiveShopGuard} from './guards/active-shop.guard';
 import {StockManagerGuard} from './guards/stock-manager.guard';
+import {LandingComponent} from './main-module/landing/landing.component';
+import {PrivacyComponent} from './main-module/privacy/privacy.component';
+import {AuthenticatedUserGuard} from './guards/authenticated-user.guard';
+import {LoginComponent} from './main-module/login/login.component';
+import {RegisterComponent} from './main-module/register/register.component';
+import {ChooseShopComponent} from './main-module/choose-shop/choose-shop.component';
 
 const routes: Routes = [
+  {path: '', component: LandingComponent},
   {
-    path: '',
-    canActivate: [],
-    loadChildren: () => import('./main-module/main-module.module').then(mod => mod.MainModuleModule)
+    path: 'privacy',
+    component: PrivacyComponent
+  },
+  {
+    path: 'login',
+    canActivate: [AuthenticatedUserGuard],
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    canActivate: [AuthenticatedUserGuard],
+    component: RegisterComponent
+  },
+  {
+    path: 'shop',
+    canActivate: [AuthenticationGuard],
+    component: ChooseShopComponent
   },
   {
     path: 'dashboard',
