fb34e9f39957713454d3d622c03ab95a0a39f467
joshuamshana <mama27j@gmail.com>
2021-07-06 14:05:24 +0300

    use npm

--- desktop.sh
+++ desktop.sh
@@ -3,6 +3,6 @@ rm -r desktop/dist
 cp -r dist/smartstock desktop
 node desktop-version.js
 cd desktop
-yarn install
-yarn build:snap
+npm run install
+npm run build:snap
 snapcraft upload --release=stable,edge,beta dist/smartstock_${PACKAGE_VERSION}_amd64.snap
