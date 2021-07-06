25cf72d30afc80812d051473024ede001e38aaa7
Joshua Mshana <mama27j@gmail.com>
2021-07-07 00:13:54 +0300

    Update desktop.sh

--- desktop.sh
+++ desktop.sh
@@ -3,6 +3,6 @@ rm -r desktop/dist
 cp -r dist/smartstock desktop
 node desktop-version.js
 cd desktop
-npm run install
+npm install
 npm run build:snap
 snapcraft upload --release=stable,edge,beta dist/smartstock_${PACKAGE_VERSION}_amd64.snap
