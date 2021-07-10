263b0fa83974c75e2c6ab0753b907297847fca6d
joshuamshana <mama27j@gmail.com>
2021-07-10 10:35:20 +0300

    update android action and auto update build version of android

---
+++ android-version.js
@@ -0,0 +1,17 @@
+console.log('-------ANDROID VERSION UPDATE---START');
+const mainPackage = require('./package.json');
+const { writeFileSync, readFileSync} = require('fs');
+const { join } = require('path');
+const versionChunks = mainPackage.version.split('.');
+const versionCode = versionChunks[versionChunks.length-1];
+const versionName = mainPackage.version;
+
+const vPath = join(__dirname, 'android', 'variables.gradle');
+let vFile  = readFileSync(vPath);
+vFile = vFile.toString()
+.replace(new RegExp('(versionCode).*', 'ig'), `versionCode = ${versionCode}`)
+.replace(new RegExp('(versionName).*','ig'), `versionName = ${versionName}`);
+writeFileSync(vPath, vFile);
+console.log(versionCode);
+console.log(versionName);
+console.log('-------ANDROID VERSION UPDATE---END');
