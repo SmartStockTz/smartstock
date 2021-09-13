51d41c3218a2a2a70f5b0b7dca0767b055a39f48
joshua <mama27j@gmail.com>
2021-09-13 12:37:41 +0200

    update bfast lib to fix cache in workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {BackgroundService} from './workers/background.service';
+import {isBrowser, isElectron} from 'bfast';
 
 @Component({
   selector: 'smartstock-root',
