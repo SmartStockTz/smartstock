0fcb5e05ea34e62f6401de90e3dd4d9bf1b86210
joshua <mama27j@gmail.com>
2021-10-10 19:33:32 +0200

    add more script in build win

---
+++ desktop/win.bat
@@ -0,0 +1,10 @@
+@REM sudo dpkg -y --add-architecture i386
+@REM wget -nc https://dl.winehq.org/wine-builds/winehq.key
+@REM sudo apt-key add -y winehq.key
+@REM sudo apt update
+@REM sudo apt install -y --install-recommends winehq-stable
+@REM
+@REM npm install -f
+@REM npm run build:win
+@REM node ./upload.win.js
+
