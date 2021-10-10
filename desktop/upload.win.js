3313285a3dc4b337210aaf01744915368fa19833
joshua <mama27j@gmail.com>
2021-10-10 20:06:39 +0200

    add more script in build win

--- desktop/upload.win.js
+++ desktop/upload.win.js
@@ -1,19 +1,17 @@
-// const bfast = require('bfast');
+const bfast = require('bfast');
 const pkg = require('./package.json');
-// const {createReadStream} = require("fs");
-// const {join} = require("path");
-const {execSync} = require("child_process");
+const {createReadStream} = require("fs");
+const {join} = require("path");
+// const {execSync} = require("child_process");
 
-// bfast.init({
-//   applicationId: 'smartstock_lb',
-//   projectId: 'smartstock'
-// });
-const r = execSync(`curl -F smartstock-${pkg.version}.exe="./dist/smartstock Setup ${pkg.version}.exe" "https://smartstock-daas.bfast.fahamutech.com/storage/smartstock_lb?pn=true"`);
-console.log(r.toString());
-// bfast.storage().save({
-//   filename: `smartstock_${pkg.version}.exe`,
-//   pn: true,
-//   data: createReadStream(join(__dirname, 'dist', `smartstock Setup ${pkg.version}.exe`))
-// }, progress => {
-//   console.log(progress);
-// }).then(console.log).catch(console.log);
+bfast.init({
+  applicationId: 'smartstock_lb',
+  projectId: 'smartstock'
+});
+bfast.storage().save({
+  filename: `smartstock_${pkg.version}.exe`,
+  pn: true,
+  data: createReadStream(join(__dirname, 'dist', `smartstock Setup ${pkg.version}.exe`))
+}, progress => {
+  console.log(progress);
+}).then(console.log);
