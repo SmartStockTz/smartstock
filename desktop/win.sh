54af9dc6d8a837286b7643bfe76955a99d6de672
joshua <mama27j@gmail.com>
2021-10-10 15:30:57 +0200

    merge with master

---
+++ desktop/win.sh
@@ -0,0 +1,8 @@
+sudo dpkg -y --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add -y winehq.key
+sudo apt update
+sudo apt install -y --install-recommends winehq-stable
+
+npm install -f
+npm run build:win
