a85f0cf15a575f741e3f8d597942e0532fcfd646
joshua <mama27j@gmail.com>
2021-12-22 16:26:24 +0100

    re activivate tawk api and return home page to products view, desktop to load from remote.

--- desktop-win.bat
+++ desktop-win.bat
@@ -1,13 +1,13 @@
-rm -r desktop/smartstock
-rm -r desktop/dist
-cp -r dist/smartstock desktop
+@REM rm -r desktop/smartstock
+@REM rm -r desktop/dist
+@REM cp -r dist/smartstock desktop
 node desktop-version.js
-cd desktop
+cd desktop || echo ""
 npm install
 npm run build:win
-npm install -g ipfs
-jsipfs init
-jsipfs daemon
-jsipfs add -r ./dist
+@REM npm install -g ipfs
+@REM jsipfs init
+@REM jsipfs daemon
+@REM jsipfs add -r ./dist
 
-#snapcraft upload --release=stable,edge,beta dist/smartstock_${PACKAGE_VERSION}_amd64.snap
+@REM #snapcraft upload --release=stable,edge,beta dist/smartstock_${PACKAGE_VERSION}_amd64.snap
